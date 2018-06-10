7b2d14b4a33dc68d2306410f9a231cd622bd2320
TheCase <case@repulsor.net>
2018-06-10 12:26:13 -0600

    bin

--- script/generate.sh
+++ script/generate.sh
@@ -4,4 +4,4 @@ cd /assets
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
 cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=client client.json | cfssljson -bare client
 cp /assets/* /certs
-cp /usr/local/cfssl* /certs
+cp /usr/local/bin/cfssl* /certs
